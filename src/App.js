119d3df4ee0ed668797fb5ba3341cccc96fe9410
Breznovic <breznovic@mail.ru>
2021-11-05 16:19:10 +0300

    New life

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import classes from './App.module.css'
-import AddTransaction from './components/AddTransaction/AddTransaction'
-import Balance from './components/Balance/Balance'
-import Header from './components/Header/Header'
-import IncomeExpenses from './components/IncomeExpenses/IncomeExpenses'
-import TransactionsList from './components/TransactionsList/TransactionsList'
-import { GlobalProvider } from './context/GlobalState'
+
 
 function App() {
   return (
-    <GlobalProvider>
-      <Header />
-      <div className={classes.container}>
-      <Balance />
-      <IncomeExpenses />
-      <TransactionsList />
-      <AddTransaction />
+
+      <div>
+
       </div>
-    </GlobalProvider>
+
   )
 }
 
