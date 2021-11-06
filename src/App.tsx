f87cb54bc87c591f3abb5efd5a535b29dfccca8a
Breznovic <breznovic@mail.ru>
2021-11-06 11:59:10 +0300

    First lesson

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import classes from './App.module.css'
+
+function App() {
+    return (
+        <div>
+            Are you work?%%%
+        </div>
+    )
+}
+
+function Rating() {
+    return (
+        <div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        </div>
+)
+}
+
+function Accordion() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
---
+++ src/ffff.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const eer = () => {
+    return (
+        <div>
+            
+            </div>
+    );
+};
+
+export default eer;
